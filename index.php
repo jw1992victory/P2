efb495c6adfceabc08147e6a74469afb26e6037e
wendy1992 <jw1992victory>
2016-09-20 21:40:42 -0400

    format code

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <div class="result"><p class="password"><?php echo $password ?></p></div>
 
     <form  method='POST' action='index.php'>
+
         <p># of Words <input type="text" name="length" value="<?php echo isset($_POST['length']) ? $_POST['length'] : '' ?>">(max of 9)</p>
         <p><input type="checkbox" name="contain_number" <?php if (!empty($_POST['contain_number'])): ?> checked="checked"<?php endif; ?>>Add a number</p>
         <p><input type="checkbox" name="contain_symbol" <?php if (!empty($_POST['contain_symbol'])): ?> checked="checked"<?php endif; ?>>Add a symbol</p>
@@ -31,8 +32,10 @@
         <?php endif ?>
 
         <input type="submit" class="button" value="Gimme another">
+
     </form>
 
     <img src="img/password_strength.png" alt="password strngth gif">
+
 </body>
 </html>
